6cce3afa598e9d6f20d6d40d6c3be3f818156bb4
Gedalya Cohen <gedco51@gmail.com>
2024-03-21 21:23:54 +0000

    trying again

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,8 +8,8 @@ export default function Header() {
   return (
     <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
       <div className='flex gap-4'>
-        <MenuItem title='home' address='/' Icon={AiFillHome} />
-        <MenuItem title='about' address='/about' Icon={BsFillInfoCircleFill} />
+        <MenuItem title='home' address="/" Icon={AiFillHome} />
+        <MenuItem title='about' address="/about" Icon={BsFillInfoCircleFill} />
       </div>
       <div className='flex items-center gap-4'>
         <DarkModeSwitch />
--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -1,22 +1,28 @@
-
-import Results from '@/components/Results';
 import { Suspense } from 'react';
+import Results from '@/components/Results';
 
-export default async function SearchPage({ params }) {
-  const seachTerm = params.searchTerm;
-  const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${seachTerm}&language=en-US&page=1&include_adult=false`
-  );
-  const data = await res.json();
-  const results = data.results;
+function fetchData(searchTerm) {
+  return fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`
+  ).then(res => res.json());
+}
+
+export default function SearchPage({ params }) {
+  const searchTerm = params.searchTerm;
+
+  // Use React suspense for data fetching
+  const data = fetchData(searchTerm);
+
   return (
-  <Suspense>
     <div>
-      {results &&
-        results.length ===
-        <h1 className='text-center pt-6'>No results found</h1>}
-      {results && <Results results={results} />}
+      <Suspense fallback={<div>Loading...</div>}>
+        {data && data.results && data.results.length === 0 ? (
+          <h1 className='text-center pt-6'>No results found</h1>
+        ) : (
+          <Results results={data.results} />
+        )}
+      </Suspense>
     </div>
-  </Suspense>
   );
 }
+
