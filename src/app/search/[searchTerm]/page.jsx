ff08a44b65c56e6504d9a493914a7e6a42aa5f65
Gedalya Cohen <gedco51@gmail.com>
2024-03-31 23:59:06 +0100

    just for a while

--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -1,29 +1,18 @@
-import Results from '@/components/results';
-import { Suspense } from 'react';
+import Results from '@/components/Results';
 
-
-function fetchData(searchTerm) {
-  return fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`
-  ).then(res => res.json());
-}
-
-export default function SearchPage({ params }) {
-  const searchTerm = params.searchTerm;
-
-  // Use React suspense for data fetching
-  const data = fetchData(searchTerm);
-
+export default async function SearchPage({ params }) {
+  const seachTerm = params.searchTerm;
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${seachTerm}&language=en-US&page=1&include_adult=false`
+  );
+  const data = await res.json();
+  const results = data.results;
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        {data && data.results && data.results.length === 0 ? (
-          <h1 className='text-center pt-6'>No results found</h1>
-        ) : (
-          <Results results={data.results} />
-        )}
-      </Suspense>
+      {results &&
+        results.length ===
+        <h1 className='text-center pt-6'>No results found</h1>}
+      {results && <Results results={results} />}
     </div>
   );
 }
-
